e10413735d1adfd44e2b95168f4005f291c5f46c
Trent Johnson <trentj@trentj.org>
2026-05-15 13:45:30 -0700

    sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -6,4 +6,8 @@
 #sudo mount -t drvfs K: /mnt/k -o uid=$(id -u $USER),gid=$(id -g $USER),metadata
 #rsync -e ssh --rsync-path="sudo rsync" -rptn --delete --progress /mnt/g/Audio/mp3/Music /mnt/k/mp3
 
-rsync -e ssh -rpt --delete --progress "/mnt/g/mp3/Music" trentj@192.168.1.200:/home/trentj/media/mp3
+# pi
+#rsync -e ssh -rpt --delete --progress "/mnt/g/mp3/Music" trentj@192.168.1.200:/home/trentj/media/mp3
+
+# LilBuddy
+rsync -e ssh -rpt --delete --progress "/mnt/i/mp3/Music" trentj@192.168.1.136:/home/trentj
